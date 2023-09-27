f4556eb99dadc787a0069f032fdb902450affe69
Gulsah Satilmis <136925048+gulsah01@users.noreply.github.com>
2023-09-27 19:14:41 +0100

    update

---
+++ src/test/java/day05_notations_Assertions/C03_BeforeClass_AfterClass.java
@@ -0,0 +1,11 @@
+package day05_notations_Assertions;
+
+public class C03_BeforeClass_AfterClass {
+    // Amazon anasayfaya gidin
+// 3 farkli test method'u olusturarak asagidaki gorevleri yapin
+// 1- Title'in Amazon icerdigini test edin
+// 2- Arama kutusuna "Nutella" yazip aratin ve sonucun Nutella icerdigini test edin
+// 3- ilk urunu tiklayin ve urun isminde Nutella gectigini test edin
+
+
+}
