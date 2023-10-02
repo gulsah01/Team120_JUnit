3394493d9df7bbfbf34fe7a1a9ef1ee3c964b129
Gulsah Satilmis <136925048+gulsah01@users.noreply.github.com>
2023-10-02 19:36:47 +0100

    update

---
+++ src/test/java/day10_actionsClass/C01_sagClick.java
@@ -0,0 +1,28 @@
+package day10_actionsClass;
+
+import org.apache.hc.core5.util.Asserts;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+public class C01_sagClick extends TestBase {
+    @Test
+    public void test01(){
+       // 2- https://the-internet.herokuapp.com/context_menu sitesine gidin
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+        //3- Cizili alan uzerinde sag click yapin
+        WebElement ciziliAlanElementi=driver.findElement(By.id("hot-spot"));
+        Actions actions =new Actions(driver);
+        actions.contextClick(ciziliAlanElementi).perform();
+       // 4- Alert’te cikan yazinin “You selected a context menu” oldugunu test edin.
+        String expectedAlertYazi="You selected a context menu";
+        String actualAlertYazi=driver.switchTo().alert().getText();
+
+       // 5- Tamam diyerek alert’i kapatalim
+        driver.switchTo().alert().accept();
+       // 6- Elemental Selenium linkine tiklayalim
+      //  7- Acilan sayfada h1 taginda “Make sure your code lands” yazdigini test edelim
+    }
+}
