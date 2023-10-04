960338a32027edf29087fd95ed7c0db1439db5c1
Gulsah Satilmis <136925048+gulsah01@users.noreply.github.com>
2023-10-04 17:28:44 +0100

    update

--- src/test/java/calisma/soru1.java
+++ src/test/java/calisma/soru1.java
@@ -4,6 +4,7 @@ import com.github.javafaker.Faker;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import utilities.TestBase;
 
 public class soru1 extends TestBase {
@@ -13,17 +14,34 @@ public class soru1 extends TestBase {
         // 1.   http://zero.webappsecurity.com/ Adresine gidin
         driver.get("http://zero.webappsecurity.com/");
         // 2.   Sign in butonuna basin
-WebElement isimKutusu =driver.findElement(By.xpath("signin_button"));
-isimKutusu.click();
+       WebElement isimKutusu =driver.findElement(By.xpath("//button[@id='signin_button']"));
+       isimKutusu.click();
         // 3.   Login kutusuna “username” yazin
-        Faker faker =new Faker();
+       WebElement login=driver.findElement(By.xpath("//input[@name='user_login']"));
+       login.sendKeys("username");
 
-        // 4.   Password kutusuna “password” yazin
+       // 4.   Password kutusuna “password” yazin
+        WebElement password =driver.findElement(By.xpath("//input[@id='user_password']"));
+        password.sendKeys("password");
+
         // 5. Sign in tusuna basin, back tusuna basarak sayfaya donun
+        driver.findElement(By.xpath("//input[@name='submit']")).click();
+        driver.navigate().back();
+
         // 6. Online banking menusunden Pay Bills sayfasina gidin
+        driver.findElement(By.xpath("//a[@id='online-banking']")).click();
+        driver.findElement(By.xpath("//span[@id='pay_bills_link']")).click();
+
         //7. “Purchase Foreign Currency” tusuna basin
+        driver.findElement(By.xpath("//a[text()='Purchase Foreign Currency']")).click();
+
         //8. “Currency” drop down menusunden Eurozone’u secin
+        WebElement dropDown =driver.findElement(By.xpath("//select[@id='pc_currency']"));
+        Select select=new Select(dropDown);
+        select.selectByVisibleText("Eurozone (euro)");
+
         //9. “amount” kutusuna bir sayi girin
+
         //10. “US Dollars” in secilmedigini test edin
         //11. “Selected currency” butonunu secin
         //12. “Calculate Costs” butonuna basin sonra “purchase” butonuna basin
