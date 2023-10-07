5b18050d1e6492da58a073781a0c59646c22883e
Gulsah Satilmis <136925048+gulsah01@users.noreply.github.com>
2023-10-07 20:48:08 +0100

    update

---
+++ src/test/java/practice/P01.java
@@ -0,0 +1,26 @@
+package practice;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import utilities.TestBase;
+
+public class P01 extends TestBase {
+
+    @Test
+    public void ahmet(){
+        System.out.println("Ahmet");
+
+    }
+    @Test @Ignore
+    public void resul(){
+        System.out.println("Resul");
+    }
+
+    @Test
+    public void nergiz(){
+        System.out.println("Nergiz");
+
+    }
+
+}
+
